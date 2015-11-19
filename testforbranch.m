497020b6f0cc548c65379266dd3d4c4f72a65f3b
Shane-C-C-Y <b99611040@ntu.edu.tw>
2015-11-19 01:07:47 -0800

    new

--- testforbranch.m
+++ testforbranch.m
@@ -1,3 +1,3 @@
-%test for branch
-%look what will happen if I add another file in a new branch then merge it
-%it should be no conflict
+%i want this file still the same in the master branch
+%even when I modify it in the branch test
+%what i need is I can go back whenever i want
