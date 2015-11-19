5e054067b7a026d4692b638c006c068b090ffc4c
Shane-C-C-Y <b99611040@ntu.edu.tw>
2015-11-19 01:10:46 -0800

    first time commit modified

--- testforbranch.m
+++ testforbranch.m
@@ -1,3 +1,7 @@
 %i want this file still the same in the master branch
 %even when I modify it in the branch test
 %what i need is I can go back whenever i want
+
+%i will modify
+%and commit
+hope that I can back even after several commit
